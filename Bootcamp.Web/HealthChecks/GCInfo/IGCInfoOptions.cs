b745dbd310c7a9010f5d0f445f14d9929468489b
Cozroy <cozroyjr@gmail.com>
2023-10-31 16:41:00 -0400

    Add project files.

---
+++ Bootcamp.Web/HealthChecks/GCInfo/IGCInfoOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Bootcamp.Web.HealthChecks
+{
+    public interface IGCInfoOptions
+    {
+        long Threshold { get; set; }
+    }
+}
---
+++ Bootcamp.Web/Models/IAuthUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Bootcamp.Web.Models
+{
+    public interface IAuthUser
+    {
+        string Status   { get; }
+        string Token    { get; }
+        string UserName { get; }
+    }
+}
